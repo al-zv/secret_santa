e36d9fe9dd80adeead86f050634a74f80d53c1ac
al-zv <al3z1v@gmail.com>
2022-11-20 19:32:18 +0500

    add factory for filling in fake data

--- database/seeders/MemberSeeder.php
+++ database/seeders/MemberSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use App\Models\Member;
 
 class MemberSeeder extends Seeder
 {
@@ -17,13 +18,18 @@ class MemberSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        for ($i = 0; $i < 40; $i++) {
+    {        
+        Member::factory()
+                ->count(40)
+                //->hasPosts(1)
+                ->create();
+
+        /*for ($i = 0; $i < 40; $i++) {
             DB::table('members')->insert([
                 'name' => Str::random(5),
                 'profession' => Str::random(8),
                 'desired_gift' => Str::random(10),
             ]);
-        }
+        }*/
     }
 }
